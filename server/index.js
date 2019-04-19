5471b2f39b0a95b789a8749d10c332c0bba44e74
Aaron Gaddes <aarongaddes@gmail.com>
2019-04-19 18:16:11 +1000

    replaced body-parser with inbuild express.json middleware

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const volleyball = require('volleyball');
-const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -8,7 +7,7 @@ const auth = require('./auth/index.js');
 
 app.use(volleyball);
 
-app.use(bodyParser.json());
+app.use(express.json());
 
 app.get('/', (req,res) => {
     res.json({
